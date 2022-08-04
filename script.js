b92d4a6bd4b178518f9893e64438b301e3bcd2e2
Phil Schmucker <p.schmuck.er@web.de>
2022-08-04 19:19:50 +0200

    aktualisierung-phil

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-async function init() {
-    await downloadFromServer();
-    allTasks = JSON.parse(backend.getItem('allTasks')) || [];
-    await includeHTML();
-    // LUKAS 04.08.22 16:06: I have commented it out because this ID (so far) is not used and causes error the console.
-    // document.getElementById('headline').innerHTML = 'Herzlich willkommen!';
-}
+// funktioniert noch nicht richtig.
+// async function init() {
+//     await downloadFromServer();
+//     allTasks = JSON.parse(backend.getItem('allTasks')) || [];
+//     await includeHTML();
+//     // LUKAS 04.08.22 16:06: I have commented it out because this ID (so far) is not used and causes error the console.
+//     // document.getElementById('headline').innerHTML = 'Herzlich willkommen!';
+// }
 
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+  let includeElements = document.querySelectorAll('[w3-include-html]');
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute('w3-include-html'); // "header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = 'Page not found';
     }
+  }
 }
