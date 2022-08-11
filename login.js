dfb009886d36b0bd32b377614636a721334add45
Phil Schmucker <p.schmuck.er@web.de>
2022-08-11 10:22:37 +0200

    userlogin

--- login.js
+++ login.js
@@ -1,23 +1,52 @@
-function login() {
-  let email = document.getElementById('inputEmail3');
-  let password = document.getElementById('inputPassword3');
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('myInput');
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus();
+});
+
+function verifyNull() {
+  let email = document.getElementById('floatingEmail').value.trim();
+  let password = document.getElementById('floatingPassword').value.trim();
+  debugger;
+  if (!email.length) {
+    console.log('Please enter email');
+  } else if (!password.length) {
+    console.log('Please enter password');
+  } else {
+    login(email, password);
+  }
+}
+
+function login(email, password) {
   let checkEmail = false;
   let checkPassword = false;
   for (let i = 0; i < users.length; i++) {
-    if (email.value == users[i].email) {
+    if (email == users[i].email) {
       checkEmail = true;
-    }
-    if (password.value == users[i].password) {
-      checkPassword = true;
+      if (password == users[i].password) {
+        checkPassword = true;
+        debugger;
+        loginAsUser(i);
+      }
     }
   }
   if (checkEmail && checkPassword) {
-    //load index.html (init)
+    window.location.href = 'index.html';
+    console.log(currentUser);
   } else if (!checkEmail) {
+    console.log('falsche email');
     toastWrongEmail();
   } else if (!checkPassword) {
+    console.log('falsches passwort');
     toastWrongPassword();
   }
+}
+
+function loginAsUser(i) {
+  currentUser = users[i];
+  localStorage.setItem('currentUser', JSON.stringify(currentUser));
+  console.log(currentUser);
 }
 
 function toastWrongEmail() {}
