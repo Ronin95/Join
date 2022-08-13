fe6c746b25f9b575d82f135baf780699de441820
Maik Langer <maiklanger@MacBook-Air-von-Maik.local>
2022-08-13 10:45:37 +0200

    add search function to search for names or category

--- backlog.js
+++ backlog.js
@@ -10,7 +10,21 @@ function generateAllTasks() {
   loadAllTasks();
   for (let i = 0; i < allTasks.length; i++) {
     let newTask = allTasks[i];
-    let newTasks = document.getElementById('freshTask');
+    let newTasks = document.getElementById("freshTask");
     newTasks.innerHTML += newTaskTemp(newTask, i);
+  }
+}
+
+function filterNames() {
+  let search = document.getElementById("searchTask").value;
+  search = search.toLowerCase();
+  document.getElementById("freshTask").innerHTML = ``;
+  for (let i = 0; i < allTasks.length; i++) {
+    let newTask = allTasks[i];
+    let name = allTasks[i].userForTask.name;
+    let category = allTasks[i].category;
+    if (name.toLowerCase().includes(search) || category.toLowerCase().includes(search))
+    {document.getElementById("freshTask").innerHTML += searchTaskTemp(newTask)
+    }
   }
 }
