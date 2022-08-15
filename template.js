deddbe8e770fa0e10d73bee4205286f53154d842
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-08-15 15:00:31 +0200

    added the delete button for backlog

--- template.js
+++ template.js
@@ -1,40 +1,53 @@
 function newTaskTemp(newTask, i) {
   return /*html*/ `
-  <div class=' backlog-task-Container'>
-    <div class='task-img-name-email '>
-      <div style="background-color: ${
-        colorsCategory[newTask.category]
-      }" class='category-task'>
+  <div class='overall-backlog'>
+    <div class=' backlog-task-Container'>
+      <div class='task-img-name-email '>
+        <div style="background-color: ${
+          colorsCategory[newTask.category]
+        }" class='category-task'>
 
-      </div>
-      <div id="user_pic">
-        <img class="user-pic m-3" src="${newTask.userForTask.avatar}">
-      </div>
-      <div id="which_user">
-        <p>${newTask.userForTask.name}</p>
-          <p>
-            <a href='mailto:invalidmail@join.com' alt='Invalid Mail Address'>
-              ${newTask.userForTask.email}
-            </a>
-          </p>
+        </div>
+        <div id="user_pic">
+          <img class="user-pic m-3" src="${newTask.userForTask.avatar}">
+        </div>
+        <div id="which_user">
+          <p>${newTask.userForTask.name}</p>
+            <p>
+              <a href='mailto:invalidmail@join.com' alt='Invalid Mail Address'>
+                ${newTask.userForTask.email}
+              </a>
+            </p>
+        </div >
       </div >
+      <div class='taskCategory'>
+        <h5>${newTask.category}</h5>
+      </div>
+      <div class='taskDescription '>
+        <p>${newTask.description}</p>
+      </div>
     </div >
-    <div class='taskCategory'>
-      <h5>${newTask.category}</h5>
+    <div class='mt-4'>
+      <img class='delete-task' onclick='deleteTask(${i})' src='img/delete-task.png' style='width: 50px;'>
     </div>
-    <div class='taskDescription '>
-      <p>${newTask.description}</p>
   </div>
-     </div >
-    `;
+  `;
+}
+
+function deleteTask(i) {
+  delete allTasks[i];
+  saveAllTasks();
+  loadAllTasks();
+  newTaskTemp();
 }
 
 function searchTaskTemp(newTask){
-  return /*html*/ `<div class=' backlog-task-Container'>
+  return /*html*/ `
+  <div class=' backlog-task-Container'>
     <div class='task-img-name-email '>
-      <div style="background-color: ${
-        colorsCategory[newTask.category]
-      }" class='category-task'>
+      <div style="background-color: 
+        ${colorsCategory[newTask.category]}"
+        class='category-task'>
       </div>
       <div id="user_pic">
         <img class="user-pic m-3" src="${newTask.userForTask.avatar}">
@@ -53,7 +66,7 @@ function searchTaskTemp(newTask){
     </div>
     <div class='taskDescription '>
       <p>${newTask.description}</p>
-  </div>
-     </div >
-    `;
+    </div>
+  </div >
+  `;
 }
