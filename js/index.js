e6515532112b314b11523be121bcb8a9a1d7a57a
Maik Langer <maiklanger@MacBook-Air-von-Maik.local>
2022-08-26 12:27:25 +0200

    added the actual date-picker in modal in index.html

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ function genHTMLBoardTaskItem(task) {
         </div>
 
         <div class="card-footer bg-transparent p-1 d-flex justify-content-between align-items-center gap-1">
-            <span class="fw-semibold">${task['date']}</span>
+            <span class="fw-semibold text-dark">${task['date']}</span>
             <div class="cardAssignedTo">
                 <img 
                   src="${task.userForTask.avatar}" 
