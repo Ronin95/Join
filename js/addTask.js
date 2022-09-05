468c8b6ca402fd5cf0b95122af3dcbec86ed19e5
Maik Langer <maiklanger@live.de>
2022-09-05 10:46:28 +0200

    makes the addTask function more readable in addTask

--- js/addTask.js
+++ js/addTask.js
@@ -1,5 +1,5 @@
 let selectedUser = [];
-let formAddTask = document.getElementById('addTaskSubmit');
+let formAddTask = document.getElementById("addTaskSubmit");
 let today = new Date();
 let dd = today.getDate();
 let mm = today.getMonth() + 1;
@@ -12,28 +12,42 @@ let yyyy = today.getFullYear();
  * show a message the Message with the value for wich Member is the task added
  */
 async function addTask() {
-  allTasks = await loadFromBackend('allTasks');
-  let title = document.getElementById('title');
-  let date = document.getElementById('txtDate');
-  let category = document.getElementById('category');
-  let urgency = document.getElementById('urgency');
-  let description = document.getElementById('description');
+  allTasks = await loadFromBackend("allTasks");
+  let date = document.getElementById("txtDate");
   let newTask = {
-    title: title.value,
+    title: getInputValue("title"),
     date: date.value,
-    category: category.value,
-    urgency: urgency.value,
-    description: description.value,
+    category: getInputValue("category"),
+    urgency: getInputValue("urgency"),
+    description: getInputValue("description"),
     userForTask: selectedUser,
     id: new Date().getTime(),
-    state: 'toDo',
+    state: "toDo",
   };
+  getDate(newTask, date);
+  saveTask(newTask);
+}
+
+/**
+ * this is a function to get the new Date from value 
+ * @param {string} newTask 
+ * @param {number} date 
+ */
+function getDate(newTask, date){
   if ((newTask.date = !date.value)) {
     newTask.date = today;
   } else {
-    newTask.date = date.value.replace('.', '/');
+    newTask.date = date.value.replace(".", "/");
   }
-  saveTask(newTask);
+}
+
+/**
+ * this is a function that return the document
+ * @param {string} id 
+ * @returns the id from the element
+ */
+function getInputValue(id) {
+  return document.getElementById(id).value;
 }
 
 /**
@@ -43,7 +57,7 @@ async function addTask() {
  */
 function saveTask(newTask) {
   allTasks.push(newTask);
-  saveInBackend(allTasks, 'allTasks');
+  saveInBackend(allTasks, "allTasks");
 }
 
 /**
@@ -60,12 +74,12 @@ function deleteUnsafedInput() {
  * creates a new element wich show only the user who is selected
  */
 function selectUser(i) {
-  document.getElementById(`selected${i}`).classList.toggle('user-selected');
+  document.getElementById(`selected${i}`).classList.toggle("user-selected");
   selectedUser = users[i];
   document.getElementById(`user`).innerHTML = /*html*/ ` 
     <div onclick="showAllUserAndDisable()"> 
       <img title="${selectedUser.name}" id="${i}" class="user-show cursor-pointer rounded-circle user-selected" src="${selectedUser.avatar}" alt=""></div>`;
-  document.getElementById('createTask').removeAttribute('disabled');
+  document.getElementById("createTask").removeAttribute("disabled");
 }
 
 /**
@@ -73,7 +87,7 @@ function selectUser(i) {
  */
 function showAllUserAndDisable() {
   showAllUser();
-  document.getElementById('createTask').disabled = true;
+  document.getElementById("createTask").disabled = true;
 }
 
 /**
@@ -83,13 +97,13 @@ function showAllUserAndDisable() {
 /* HAVE TO REWRITE for the new BOOTSRAP DATE*/
 function loadCurrentDate() {
   if (/^\d$/.test(dd)) {
-    dd = '0' + dd;
+    dd = "0" + dd;
   }
   if (/^\d$/.test(mm)) {
-    mm = '0' + mm;
+    mm = "0" + mm;
   }
-  today = dd + '/' + mm + '/' + yyyy;
-  document.getElementById('txtDate').value = today;
+  today = dd + "/" + mm + "/" + yyyy;
+  document.getElementById("txtDate").value = today;
   // document.getElementById('today').innerHTML += /*html*/ `
   // <div class="today-date d-flex justify-content-center flex-column">
   //   <span class="d-flex justify-content-end ">TODAY</span>
@@ -99,7 +113,7 @@ function loadCurrentDate() {
 }
 
 function handleForm(event) {
-  const successToast = document.getElementById('success_task');
+  const successToast = document.getElementById("success_task");
   event.preventDefault();
   const toast = new bootstrap.Toast(successToast);
   toast.show();
@@ -108,4 +122,4 @@ function handleForm(event) {
   console.log(today, dd.length, mm.length);
   loadCurrentDate();
 }
-formAddTask.addEventListener('submit', handleForm);
+formAddTask.addEventListener("submit", handleForm);
